d937a2b1d32d73b7872fb5906d0a3a0bf1b9fa5c
chaheein123 <chaheein@gmail.com>
2026-04-11 18:30:11 -0700

    first commit

---
+++ systems_performance.py
@@ -0,0 +1,38 @@
+# kernel, process, thread, main memory, virtual memory, context switch, mode switch, syscall, 
+
+class Core:
+    def __init__(self, name):
+        self.hardware_thread_1 = HardwareThread(f"{name}-hardware_thread_1")
+        self.hardware_thread_2 = HardwareThread(f"{name}-hardware_thread_2")
+
+
+class HardwareThread:
+    def __init__(self, name):
+        self.name = name
+        self.mode = "user_mode"
+
+
+class PlatformThread:
+    def __init__(self):
+        pass
+
+class Proc:
+    def __init__(self, task):
+        self.task = task
+    def begin_task(self):
+        process_thread = PlatformThread()
+        
+class MainMem:
+    mem = [None] * 10
+    @classmethod
+    def add_to_mem(cls, proc):
+        cls.mem.append(proc)
+        
+
+
+
+
+my_core = Core("C1")
+print(my_core.hardware_thread_1.name)
+MainMem.add_to_mem("awa")
+print(MainMem().mem)
