726b934f6a22e5641f16e5af902bca8b52d21df4
Ian Cha <chaheein@gmail.com>
2026-05-09 14:15:45 -0700

    Update persistent_storage.py

--- persistent_storage.py
+++ persistent_storage.py
@@ -75,17 +75,22 @@ class Kernel:
             }
         }
 
-        self.file_mapping_lba = {
-            "a.txt": {"lba": 0, "fs_type": "EXT", "action": self.read_ext},
-            "b.txt": {"lba": 1, "fs_type": "EXT", "action": self.read_ext},
-            "c.txt": {"lba": 2, "fs_type": "EXT", "action": self.read_ext},
-            "d.txt": {"lba": 3, "fs_type": "FAT", "action": self.read_ext},
-            "e.txt": {"lba": 4, "fs_type": "EXT", "action": self.read_ext},
-            "f.txt": {"lba": 5, "fs_type": "EXT", "action": self.read_ext},
-        }
+        self.file_mapping_lba = {}
+        # self.file_mapping_lba = {
+        #     "a.txt": {"lba": 0, "fs_type": "EXT", "read": self.read_ext, "write": self.write_ext},
+        #     "b.txt": {"lba": 1, "fs_type": "EXT", "read": self.read_ext, "write": self.write_ext},
+        #     "c.txt": {"lba": 2, "fs_type": "EXT", "read": self.read_ext, "write": self.write_ext},
+        #     "d.txt": {"lba": 3, "fs_type": "FAT", "read": self.read_ext, "write": self.write_ext},
+        #     "e.txt": {"lba": 4, "fs_type": "EXT", "read": self.read_ext, "write": self.write_ext},
+        #     "f.txt": {"lba": 5, "fs_type": "EXT", "read": self.read_ext, "write": self.write_ext},
+        # }
+    def create_file(self,file_name, fs_type):
+        if file_name not in self.file_mapping_lba:
+            self.file_mapping_lba[file_name] = {"fs_type": fs_type}
+        pass
+
     def read_ext(self, inode_table):
         # print(inode_table, len(c), "yoyoyo")
-        key = "blockpointers:"
         start = inode_table.find("blockpointers:[") + len("blockpointers:[")
         end = inode_table.find("]", start)
         raw_value = inode_table[start:end] # This gives you the string "1"
@@ -94,12 +99,20 @@ class Kernel:
         # .split(",") handles multiple items, int(x) makes them numbers
         block_list = [int(x) for x in raw_value.split(",") if x.strip()]
         return(self.diskcontroller.get_data(block_list))
+    
+    def write_ext(self, inode_table, data):
+        print("hello", inode_table, data)
+        pass
 
     def read_file(self, file_name):
         lba_index = self.file_mapping_lba[file_name]["lba"]
-        # self.read_ext()
         inode_table = self.diskcontroller.get_data([lba_index])
-        return(self.file_mapping_lba[file_name]["action"](inode_table))
+        return(self.file_mapping_lba[file_name]["read"](inode_table))
+    
+    def write_data(self, file_name, data):
+        lba_index = self.file_mapping_lba[file_name]["lba"]
+        inode_table = self.diskcontroller.get_data([lba_index])
+        return(self.file_mapping_lba[file_name]["write"](inode_table, data))
     
     # def write_file(self, file_name, data):
     #     lba_index = self.file_mapping_lba[file_name]
@@ -120,16 +133,22 @@ class DiskController:
             data_result += page.data
             return data_result
     
-    def write_data(self, lba_index, file_name, data):
-        block_page_index = self.ssd.lba[lba_index]
-        block_index = block_page_index["block"]
-        page_index = block_page_index["page"]
+    # def write_new_data(self, file_name, data):
+        # write_mapping = {
+        #     ""
+        # }
+        # lba_index = self.ss
+        # pass
+        # block_page_index = self.ssd.lba[lba_index]
+        # block_index = block_page_index["block"]
+        # page_index = block_page_index["page"]
 
 
     
 class Ssd:
     def __init__(self):
         self.plane = [Block(i) for i in range(100)]
+        # self.plane[0]
 
         # lba's mapping is this: lba key -> PBA
         # self.lba = {1: {"block": 0, "page": 0}, 2: {"block": 0, "page": 1}, 3: {"block": 0, "page": 2}, 4: {"block": 0, "page": 3}}
@@ -164,8 +183,8 @@ class Page:
 
         # if self.page_id < 5
 
-        # self.data = ""
-        self.data = "author:Ian Cha,permission:[read,write],uid:3,gid:32,filesize:5102,blockpointers:[1]"
+        self.data = ""
+        # self.data = "author:Ian Cha,permission:[read,write],uid:3,gid:32,filesize:5102,blockpointers:[1]"
         self.is_empty = True
     
     def __str__(self):
@@ -184,9 +203,11 @@ my_ssd = Ssd()
 my_diskcontroller = DiskController(my_ssd)
 my_kernel = Kernel(my_diskcontroller)
 
-# Kernel writes data
-# my_kernel.write_file("a.txt", )
 
-# Kernel finds out that a.txt is at LBA 0.  
-print(my_kernel.read_file("a.txt"))
-# print(my_ssd.plane)
+# Kernel finds out that a.txt is at LBA 0.
+# my_kernel.create_file("a.txt")
+# print(my_kernel.read_file("a.txt"))
+# my_kernel.write_data("a.txt", "abcdef")
+# my_kernel.delete_all("a.txt")
+# my_kernel.delete_data("a.txt")
+
