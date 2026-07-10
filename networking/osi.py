36bcf97d63589c12399bd9c3078d9d241dd808ad
Ian Cha <chaheein@gmail.com>
2026-07-09 17:33:39 -0700

    Update osi.py

--- networking/osi.py
+++ networking/osi.py
@@ -1,3 +1,7 @@
+# Design a system where different classes are: router, your own laptop, other company servers
+# a data packet will be passed from each object and encasulate/decasulate the data packets.
+
+
 class VFS:
     def __init__(self):
         pass
